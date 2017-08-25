8a2c5d4123153e8847b361149790e838cff14b83
Sidsingh <siddharthsingh.hbtik@gmail.com>
2017-08-25 15:19:54 +0000

    with changes to recipes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -1,1 +1,12 @@
 #/bin/bash: wq: command not found
+#
+ package 'apache2' do
+	package_name 'httpd'
+	action :install
+end
+
+service 'apache2' do
+	service_name 'httpd'
+	action [:start, :enable]
+end
+
--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,1 +1,4 @@
-
+file 'www website' do
+	path 'var/www/html/index.html'
+	content "hello world!"
+end
